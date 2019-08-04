a02b64228274caeb3239baa39ba41d77789e8194
Bożena Prońska <bozena.pronska@wp.pl>
2019-08-04 13:06:47 +0200

    build

---
+++ build/precache-manifest.af231bd09f7b69d8ad1ca919fc35a79b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ff4fd7ab0be225c3a83149d87e2d9c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9a000fa63b7459a40729",
+    "url": "./static/css/main.f1513501.chunk.css"
+  },
+  {
+    "revision": "c95a11ebc71df31edca6",
+    "url": "./static/js/2.e2132599.chunk.js"
+  },
+  {
+    "revision": "9a000fa63b7459a40729",
+    "url": "./static/js/main.5cef8422.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
